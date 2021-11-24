c0b2e5438fa0e4971dbec46506b7d0833b266eb8
Achille Monod <achille.monod&@gmail.com>
2021-11-24 15:04:40 +0100

    ajout views

--- uber-boot/src/main/java/ubereat/model/Views.java
+++ uber-boot/src/main/java/ubereat/model/Views.java
@@ -2,4 +2,33 @@ package ubereat.model;
 
 public class Views {
 
+	public static class ViewCommon {
+	}
+	
+	public static class ViewArticle extends ViewCommon {
+	}
+	
+	public static class ViewClient extends ViewCommon {
+	}
+	
+	public static class ViewCommande extends ViewCommon {
+	}
+	
+	public static class ViewLivreur extends ViewCommon {
+	}
+	
+	public static class ViewRestaurant extends ViewCommon {
+	}
+	
+	public static class ViewRestaurateur extends ViewCommon {
+	}
+	
+	public static class ViewTypePlat extends ViewCommon {
+	}
+	
+	public static class ViewTypeResto extends ViewCommon {
+	}
+	
+	public static class ViewUtilisateur extends ViewCommon {
+	}
 }
