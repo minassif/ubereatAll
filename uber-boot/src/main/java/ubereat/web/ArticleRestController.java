b11308ee9ccab19b9b5243b1e7e13b16d0a390af
NASSIF <michelnassif1994@gmail.com>
2021-11-24 15:07:41 +0100

    controller

--- uber-boot/src/main/java/ubereat/web/ArticleRestController.java
+++ uber-boot/src/main/java/ubereat/web/ArticleRestController.java
@@ -1,5 +1,78 @@
 package ubereat.web;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+
+
+import ubereat.model.Article;
+import ubereat.repository.IArticle;
+
+@RestController
+@RequestMapping("/article")
+@CrossOrigin("*")
 public class ArticleRestController {
+	
+	
+	@Autowired
+	private IArticle articleRepo;
+	
+	@GetMapping("")
+	public List<Article> findAll() {
+		List<Article> articles = articleRepo.findAll();
+
+		return articles;
+	}
+	
+	@GetMapping("{id}")
+	public Article find(@PathVariable Long id) {
+		Optional<Article> optArticle = articleRepo.findById(id);
+
+		if (optArticle.isPresent()) {
+			return optArticle.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Article non trouvé");
+		}
+	}
+	
+	@PostMapping("")
+	public Article create(@RequestBody Article article) {
+		article = articleRepo.save(article);
+
+		return article;
+	}
+
+	@PutMapping("/{id}")
+	public Article update(@PathVariable Long id, @RequestBody Article article) {
+		if (!articleRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Evaluation non trouvé");
+		}
+
+		article = articleRepo.save(article);
+
+		return article;
+	}
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if (!articleRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Evaluation non trouvé");
+		}
+		
+		articleRepo.deleteById(id);
+	}
 
 }
--- uber-boot/src/main/java/ubereat/web/ClientRestController.java
+++ uber-boot/src/main/java/ubereat/web/ClientRestController.java
@@ -1,5 +1,87 @@
 package ubereat.web;
 
-public class ClientRestController {
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+
+
+import ubereat.model.Client;
+import ubereat.repository.IClient;
+
+@RestController
+@RequestMapping("/client")
+@CrossOrigin("*")
+public class ClientRestController {
+	
+	@Autowired
+	private IClient clientRepo;
+	
+	
+	
+
+	@GetMapping("")
+	public List<Client> findAll() {
+		List<Client> clients = clientRepo.findAll();
+		return clients;
+	}
+	
+	
+	@GetMapping("{id}")
+	public Client find(@PathVariable Long id) {
+		Optional<Client> optClient = clientRepo.findById(id);
+
+		if (optClient.isPresent()) {
+			return optClient.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client non trouvé");
+		}
+	}
+	
+	@PostMapping("")
+
+	public Client create(@RequestBody Client client) {
+		client = clientRepo.save(client);
+
+		return client;
+	}
+	
+	
+	@PutMapping("/{id}")
+
+	public Client update(@PathVariable Long id, @RequestBody Client client) {
+		if (!clientRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client non trouvé");
+		}
+
+		client = clientRepo.save(client);
+
+		return client;
+	}
+
+	
+	
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if (!clientRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Client non trouvé");
+		}
+		
+		clientRepo.deleteById(id);
+	}
+	
+	
 }
