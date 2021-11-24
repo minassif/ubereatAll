c234a9c34fe2746789119ed099221c9706617001
aymeric <aymeric-dehorter@hotmail.fr>
2021-11-24 15:05:52 +0100

    controller typeplat typerest utilisateur

--- uber-boot/src/main/java/ubereat/web/TypePlatRestController.java
+++ uber-boot/src/main/java/ubereat/web/TypePlatRestController.java
@@ -1,5 +1,99 @@
 package ubereat.web;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ubereat.model.TypePlat;
+import ubereat.model.TypeResto;
+import ubereat.repository.ITypePlat;
+
+@RestController
+@RequestMapping("/TypePlat")
+@CrossOrigin("*")
 public class TypePlatRestController {
+	
+	
+
+		@Autowired
+		private ITypePlat TypePlatRepo;
+
+		@GetMapping("")
+		
+		public List<TypePlat> findAll() {
+			List<TypePlat> TypePlats = TypePlatRepo.findAll();
+
+			return TypePlats;
+		}
+		
+		
+
+		@GetMapping("{id}")
+		public TypePlat find(@PathVariable Long id) {
+			Optional<TypePlat> optTypePlat = TypePlatRepo.findById(id);
+
+			if (optTypePlat.isPresent()) {
+				return optTypePlat.get();
+			} else {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TypePlat non trouvé");
+			}
+		}
+		
+		@GetMapping("/Like{String}")
+		public List<TypePlat> findByName(@PathVariable String nom) {
+			List<TypePlat> optTypeResto = TypePlatRepo.findByName(nom);
+
+			if (!optTypeResto.isEmpty()) {
+				return optTypeResto;
+			} else {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TypeResto non trouvé");
+			}
+		}
+
+		@PostMapping("")
+		public TypePlat create(@Valid @RequestBody TypePlat TypePlat, BindingResult result) {
+			
+			
+			TypePlat = TypePlatRepo.save(TypePlat);
+
+			return TypePlat;
+		}
+
+		@PutMapping("/{id}")
+		public TypePlat update(@PathVariable Long id, @RequestBody TypePlat TypePlat) {
+			if (!TypePlatRepo.existsById(id)) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TypePlat non trouvé");
+			}
+
+			TypePlat = TypePlatRepo.save(TypePlat);
+
+			return TypePlat;
+		}
+		
+		@DeleteMapping("/{id}")
+		public void delete(@PathVariable Long id) {
+			if (!TypePlatRepo.existsById(id)) {
+				throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TypePlat non trouvé");
+			}
+			
+			TypePlatRepo.deleteById(id);
+		}
 
 }
+
+
--- uber-boot/src/main/java/ubereat/web/TypeRestoRestController.java
+++ uber-boot/src/main/java/ubereat/web/TypeRestoRestController.java
@@ -1,5 +1,98 @@
 package ubereat.web;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ubereat.model.TypeResto;
+import ubereat.repository.ITypeResto;
+
+@RestController
+@RequestMapping("/TypeResto")
+@CrossOrigin("*")
 public class TypeRestoRestController {
 
+
+
+	@Autowired
+	private ITypeResto TypeRestoRepo;
+
+	@GetMapping("")
+
+	public List<TypeResto> findAll() {
+		List<TypeResto> TypeRestos = TypeRestoRepo.findAll();
+
+		return TypeRestos;
+	}
+
+
+
+	@GetMapping("{id}")
+	public TypeResto find(@PathVariable Long id) {
+		Optional<TypeResto> optTypeResto = TypeRestoRepo.findById(id);
+
+		if (optTypeResto.isPresent()) {
+			return optTypeResto.get();
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TypeResto non trouvé");
+		}
+	}
+	
+	@GetMapping("/Like{String}")
+	public List<TypeResto> findByName(@PathVariable String nom) {
+		List<TypeResto> optTypeResto = TypeRestoRepo.findByName(nom);
+
+		if (!optTypeResto.isEmpty()) {
+			return optTypeResto;
+		} else {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TypeResto non trouvé");
+		}
+	}
+
+	@PostMapping("")
+	public TypeResto create(@Valid @RequestBody TypeResto TypeResto, BindingResult result) {
+
+
+		TypeResto = TypeRestoRepo.save(TypeResto);
+
+		return TypeResto;
+	}
+
+	@PutMapping("/{id}")
+	public TypeResto update(@PathVariable Long id, @RequestBody TypeResto TypeResto) {
+		if (!TypeRestoRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TypeResto non trouvé");
+		}
+
+		TypeResto = TypeRestoRepo.save(TypeResto);
+
+		return TypeResto;
+	}
+
+	@DeleteMapping("/{id}")
+	public void delete(@PathVariable Long id) {
+		if (!TypeRestoRepo.existsById(id)) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "TypeResto non trouvé");
+		}
+
+		TypeRestoRepo.deleteById(id);
+	}
+
 }
+
+
