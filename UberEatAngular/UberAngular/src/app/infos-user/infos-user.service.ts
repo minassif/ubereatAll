26f4c98db0473dfa139ddd5c33a38cf03cf1fba6
Dehorter Aymeric <aymeric-dehorter@hotmail.fr>
2021-12-04 18:14:03 +0100

    form

--- UberEatAngular/UberAngular/src/app/infos-user/infos-user.service.ts
+++ UberEatAngular/UberAngular/src/app/infos-user/infos-user.service.ts
@@ -2,8 +2,9 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { AppConfigService } from '../app-config.service';
 import { ConnectService } from '../connect/connect.service';
-import { Client } from '../model';
+import { Client, Livreur, Restaurateur } from '../model';
 import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +12,12 @@ import { Observable } from 'rxjs';
 export class InfosUserService {
 
   statut: string;
+  userUrl: string;
 
-  constructor(private http: HttpClient, private appConfig: AppConfigService, public ConnectService: ConnectService) { 
+  constructor(private http: HttpClient, private appConfig: AppConfigService, public ConnectService: ConnectService,private router:Router) { 
+
+    this.userUrl=this.appConfig.backEndUrl;
+
     if (ConnectService.utilisateur){
      if (ConnectService.utilisateur.statut ==='client'){
        this.statut='client';
@@ -26,8 +31,22 @@ export class InfosUserService {
     }
   }
 
-  createClient(client:Client): Observable<Client> {
-    return this.http.post<Client>(this.http + "client", client);
+  createClient(client:Client) {
+    this.http.post<Client>(this.userUrl + "client", client).subscribe(resp => {
+      this.router.navigate(["/rechercheResto"]);},
+      err => console.log(err));
+  }
+
+  createRestaurateur(restaurateur:Restaurateur) {
+    this.http.post<Restaurateur>(this.userUrl + "restaurateur", restaurateur).subscribe(resp => {
+      this.router.navigate(["/"]);},
+      err => console.log(err));
+  }
+
+  createLivreur(livreur:Livreur) {
+    this.http.post<Livreur>(this.userUrl + "livreur", livreur).subscribe(resp => {
+      this.router.navigate(["/"]);},
+      err => console.log(err));
   }
 
 }
