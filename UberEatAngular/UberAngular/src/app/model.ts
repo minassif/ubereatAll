02c8c4cde400b4cd62fa3143ee011112f2e0a867
Dehorter Aymeric <aymeric-dehorter@hotmail.fr>
2021-12-06 12:18:26 +0100

    PUSH

--- UberEatAngular/UberAngular/src/app/model.ts
+++ UberEatAngular/UberAngular/src/app/model.ts
@@ -153,6 +153,7 @@ export class Article {
     description: string;
     img: string;
     typePlat: string;
+    restaurant: Restaurant;
 
     constructor(id?: number, version?: number,vegetarien?:boolean, nom?: string, prix?: number, description?: string, img?: string, typePlat?:string) {
         this.id = id;
