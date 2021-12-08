b603f23c5217032e361f014de0dae4f658a9c503
Dehorter Aymeric <aymeric-dehorter@hotmail.fr>
2021-12-08 16:43:54 +0100

    ok

--- UberEatAngular/UberAngular/src/app/panier/panier.service.ts
+++ UberEatAngular/UberAngular/src/app/panier/panier.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { AppConfigService } from '../app-config.service';
 import { ConnectService } from '../connect/connect.service';
 import { Article, Client, Commande } from '../model';
@@ -11,64 +11,70 @@ import { ClientService } from '../service_client/client.service';
 })
 export class PanierService {
 
-  commande:Commande=new Commande();
-  panier:Array<Article>=new Array<Article>();
-  prixPanier:number=0;
-  client:Client=new Client();
+  commande: Commande = new Commande();
+  panier: Array<Article> = new Array<Article>();
+  prixPanier: number = 0;
+  client: Client = new Client();
 
-  constructor(private http: HttpClient,private appConfig:AppConfigService, private router:Router,private connectService:ConnectService,private clientService:ClientService) { 
-    
+  constructor(private http: HttpClient, private appConfig: AppConfigService, private router: Router, private connectService: ConnectService, private clientService: ClientService) {
+
   }
 
-  show():Array<Article>{
+  show(): Array<Article> {
     return this.panier
   }
 
-  findClient():Client{
+  findClient(): Client {
     return this.client
   }
 
-  add(article:Article){
+  add(article: Article) {
     this.setPrix(article.prix);
     this.panier.push(article)
   }
 
-  delete(index:number){
+  delete(index: number) {
     this.setPrix(-this.panier[index].prix);
-    this.panier.splice(index,1);
+    this.panier.splice(index, 1);
   }
 
-  setPrix(prixArticle:number){
-    this.prixPanier+=prixArticle;
+  setPrix(prixArticle: number) {
+    this.prixPanier += prixArticle;
   }
 
-  validerCommande(panier:Array<Article>){
-    this.commande.prix=this.prixPanier;
-    this.commande.articles=this.panier;
-    this.commande.client=this.client;
-    this.commande.restaurant=this.panier[0].restaurant;
-    this.commande.status='Enregistre';
+  validerCommande(panier: Array<Article>) {
+    this.commande.prix = this.prixPanier;
+    this.commande.articles = this.panier;
+    this.commande.restaurant = this.panier[0].restaurant;
+    this.commande.status = 'Enregistre';
     this.saveCommande(this.commande);
-    this.commande=null;
-    this.panier=null;
-    this.prixPanier=0;
+    this.commande = null;
+    this.panier = null;
+    this.prixPanier = 0;
   }
 
-  saveCommande(commande : Commande) {
-    if (!this.connectService.utilisateur){
+  saveCommande(commande: Commande) {
+    if (!this.connectService.utilisateur) {
       this.router.navigate(['/connect']);
+    } else {
+      this.clientService.findById(this.connectService.utilisateur.id).subscribe(resp => {
+        this.client = resp;
+        commande.client = this.client;
+
+        this.http.post<Commande>(this.appConfig.backEndUrl + 'commande', commande).subscribe(resp => {
+          this.router.navigate(['/mesCommandes']);
+        }, error => console.log(error));
+      }, err => console.log(err));
     }
-    this.loadClient();
-    this.http.put<Commande>(this.appConfig.backEndUrl+'commande/'+commande.id, commande).subscribe(resp => {
-      this.router.navigate(['/mesCommandes']);
-    }, error => console.log(error));
+
+
   }
 
-  loadClient(){
-    this.clientService.findById(this.connectService.utilisateur.id).subscribe(resp =>{
-      this.client=resp;
-    },err => console.log(err))
+  loadClient() {
+    this.clientService.findById(this.connectService.utilisateur.id).subscribe(resp => {
+      this.client = resp;
+    }, err => console.log(err));
   }
 
-  
+
 }
