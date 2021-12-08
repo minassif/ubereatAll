1c61b73be0382b27b258f84a6b6b5693b9cb2f74
Dehorter Aymeric <aymeric-dehorter@hotmail.fr>
2021-12-08 14:38:51 +0100

    adresse de livraison

--- UberEatAngular/UberAngular/src/app/panier/panier.component.ts
+++ UberEatAngular/UberAngular/src/app/panier/panier.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ArticleService } from '../article.service';
 import { ConnectService } from '../connect/connect.service';
 import { Article, Client } from '../model';
+import { ClientService } from '../service_client/client.service';
 import { PanierService } from './panier.service';
 
 @Component({
@@ -13,10 +14,12 @@ export class PanierComponent implements OnInit {
 
   panier:Array<Article>=new Array<Article>();
   client:Client=new Client();
-  clicli:boolean;
+  clicli:boolean=false;
 
-  constructor(private connectService:ConnectService,private articleService:ArticleService,private panierService: PanierService) { 
-    this.connected();
+  constructor(public connectService:ConnectService,private articleService:ArticleService,private panierService: PanierService,private clientService:ClientService) { 
+    if(this.connectService.utilisateur){
+      this.loadClient();
+    }
   }
 
   ngOnInit(): void {
@@ -25,23 +28,19 @@ export class PanierComponent implements OnInit {
   findClient(){
     return this.client
   }
-
-  connected(){
-    if(this.connectService.utilisateur){
-      this.panierService.loadClient();
-      this.client=this.panierService.client;
-      this.clicli= true
-    }
-    else{
-      this.clicli= false
-    }
+  loadClient(){
+    this.clientService.findById(this.connectService.utilisateur.id).subscribe(resp =>{
+      this.client=resp;
+    },err => console.log(err))
   }
 
-   showPanier():Array<Article>{
-     return this.panierService.panier
-   }
 
-   delete(index:number){
+  
+  showPanier():Array<Article>{
+     return this.panierService.panier
+  }
+
+  delete(index:number){
     this.panierService.delete(index);
   }
 
