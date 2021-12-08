8825f927fceeb42c19f6bd16cc372e9a5a75fd8e
Achille Monod <achille.monod&@gmail.com>
2021-12-08 10:50:44 +0100

    accueil lien carte

--- UberEatAngular/UberAngular/src/app/acceuil/acceuil.component.ts
+++ UberEatAngular/UberAngular/src/app/acceuil/acceuil.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Restaurant } from '../model';
+import { RechercheRestoService } from '../recherche-resto/recherche-resto.service';
 import { RestaurantService } from '../service_restaurant/restaurant.service';
 
 @Component({
@@ -14,11 +16,15 @@ export class AcceuilComponent implements OnInit {
 
 
 
-  constructor(private restaurantService:RestaurantService) { }
+  constructor(private restaurantService:RestaurantService,private rechercheRestoService:RechercheRestoService, private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  setResto(id:number){
+    this.rechercheRestoService.idRestoVisible=id;
+    this.router.navigate(['/carteResto']);
+  }
 
   listBestRates():Array<Restaurant>{
     let remove=this.restaurantService.findAllOrderByRate().length-3;
